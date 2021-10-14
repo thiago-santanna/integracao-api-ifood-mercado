d5fa03bf6267fe03e2201522081f398bf128a5e2
xThiagoSant <uzmaki_thiago@outlook.com>
2021-10-14 18:06:23 -0300

    primeira versao do modelo para o banco da tabela produto_integra

--- src/main/java/com/sigma/ifood/ifoodMercadoApi/models/ProdutoIntegra.java
+++ src/main/java/com/sigma/ifood/ifoodMercadoApi/models/ProdutoIntegra.java
@@ -2,30 +2,52 @@ package com.sigma.ifood.ifoodMercadoApi.models;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "produto_integra")
 public class ProdutoIntegra {
-	private Long idLoja;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(length = 25)
+	private Long idLoja;	
+	@Column(length = 100)
 	private String departamento;
+	@Column(length = 100)
 	private String categoria;
+	@Column(length = 100)
 	private String subCategoria;
+	@Column(length = 100)
 	private String marca;
+	@Column(length = 100)
 	private String unidade;
+	@Column(length = 100)
 	private String volume;
-	private String codigoBarra;
-	private String nome;
+	private Integer codigoBarra;
+	@Column(length = 150)
+	private String nome;	
 	private BigDecimal valor;
 	private BigDecimal valorPromocao;
 	private BigDecimal valorAtacado;
 	private BigDecimal valorCompra;
 	private BigDecimal quantidadeEstoqueAtual;
 	private BigDecimal quantidadeEstoqueMinimo;
-	private BigDecimal quantidadeAtacado;
+	private BigDecimal quantidadeAtacado;	
+	@Column(columnDefinition = "TEXT")
 	private String descricao;
 	private Boolean ativo;
 	private Integer plu;
 	private Boolean validadeProxima;
+	@Column(length = 150)
 	private String imageURL;	
 	
 }
