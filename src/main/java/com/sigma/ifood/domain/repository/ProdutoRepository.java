72415db7f72ad8096e8272a3dae2d421a87ce0ff
xThiagoSant <uzmaki_thiago@outlook.com>
2021-10-17 05:54:58 -0300

    criacao do repositorio de produtos com uma consulta personalizada

---
+++ src/main/java/com/sigma/ifood/domain/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.sigma.ifood.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sigma.ifood.domain.models.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	List<Produto> findByIntegrar(Boolean integrar);
+}
