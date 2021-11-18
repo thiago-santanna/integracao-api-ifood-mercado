156a2bdfe05c3d77a69065df4a7faad70654f765
xThiagoSant <uzmaki_thiago@outlook.com>
2021-11-18 14:50:35 -0300

    ajustes de controle nas respostas de metodos

--- src/main/java/com/sigma/ifood/schedule/AgendamentoService.java
+++ src/main/java/com/sigma/ifood/schedule/AgendamentoService.java
@@ -25,80 +25,79 @@ import com.sigma.ifood.ifoodMercadoApi.service.VerificarEventoService;
 
 @Service
 @EnableScheduling
-public class AgendamentoService {		
+public class AgendamentoService {
 	private final long SEGUNDO = 1000;
 	private final long CINCO_SEGUNDOS = 5000;
 	private final long DEZ_SEGUNDOS = SEGUNDO * 10;
 	private final long VINTE_SEGUNDOS = SEGUNDO * 20;
-	private final long TRINTA_SEGUNDOS = SEGUNDO * 30;	
+	private final long TRINTA_SEGUNDOS = SEGUNDO * 30;
 	private final long MINUTO = SEGUNDO * 60;
-	private final long MEIA_HORA = MINUTO * 30; 
+	private final long MEIA_HORA = MINUTO * 30;
 	private final long HORA = MINUTO * 60;
-	
+
 	@Autowired
 	private BuscarTokenValido buscarToken;
-	
+
 	@Autowired
 	private BuscaEventoPedidosService buscarEventosService;
-	
+
 	@Autowired
 	private EventosService eventosService;
-	
+
 	@Autowired
 	private ProdutoDomainService produtoDomainService;
-	
+
 	@Autowired
 	private ProductDomainAssembler productDomainAssembler;
-	
+
 	@Autowired
-	private IntegrarProdutoService integrarProdutoService;	
-	
+	private IntegrarProdutoService integrarProdutoService;
+
 	@Autowired
 	private VerificarEventoService verificarEventService;
-	
+
 	@Scheduled(fixedDelay = CINCO_SEGUNDOS, initialDelay = MEIA_HORA)
 	public void verificarEventos() {
-		
-		//Pegando lista de eventos
+
+		// Pegando lista de eventos
 		List<Events> eventos = buscarEventosService.getEventos(buscarToken.getTokenValid());
-		
-		if(eventos != null) {		
+
+		if (eventos != null) {
 			ObjectMapper mapper = new ObjectMapper();
-			
-			List<Eventos> domainEventos = eventos.stream()
-				.map(evt -> mapper.convertValue(evt, Eventos.class))
-				.collect(Collectors.toList());
-			
-			//Salvando a lista no banco de dados
-			eventosService.salvar(domainEventos);
-			
-			//Verificando(limpando) a lista de eventos na api
-			List<PedidoVerificado> pedidosVerificados = eventos.stream()
-					.map(evt -> new PedidoVerificado(evt.getId()))
+
+			List<Eventos> domainEventos = eventos.stream().map(evt -> mapper.convertValue(evt, Eventos.class))
 					.collect(Collectors.toList());
-			
-			verificarEventService.verificaPedido(pedidosVerificados, buscarToken.getTokenValid());
-			
+
+			// Salvando a lista no banco de dados
+			List<Eventos> EventsSaved = eventosService.salvar(domainEventos);
+
+			// Verificando/Limpando a lista de eventos da api
+			if (EventsSaved != null) {
+				List<PedidoVerificado> pedidosVerificados = EventsSaved.stream()
+						.map(evt -> new PedidoVerificado(evt.getId())).collect(Collectors.toList());
+
+				verificarEventService.verificaPedido(pedidosVerificados, buscarToken.getTokenValid());
+			}
 		}
 	}
-	
-	
-	@Scheduled(fixedDelay = MINUTO, initialDelay = DEZ_SEGUNDOS) public
-	void integrarProdutos() {
-		System.out.println("Serviço de integração de produto executado "); 
+
+	@Scheduled(fixedDelay = MINUTO, initialDelay = DEZ_SEGUNDOS)
+	public void integrarProdutos() {
+		System.out.println("Serviço de integração de produto executado ");
 		List<ProdutoDomain> lisOfProductIntegrable = produtoDomainService.lisOfProductIntegrable();
 		List<Produto> produtos = productDomainAssembler.toProdutoIfoodMercado(lisOfProductIntegrable);
-		
-		//TESTE nodejs como api, sem TOKEN
+
+		// TESTE nodejs como api
 		integrarProdutoService.integrarProdutos("", produtos);
-		//integrarProdutoService.integrarProdutos(buscarToken.getTokenValid(), produtos);
-		
+		// integrarProdutoService.integrarProdutos(buscarToken.getTokenValid(),
+		// produtos);
+
 		for (ProdutoDomain produto : lisOfProductIntegrable) {
 			produto.setDataUltimaItegracao(LocalDateTime.now());
 			produto.setIntegrar(false);
 		}
-		
+
 		produtoDomainService.updatedProductsIntegrated(lisOfProductIntegrable);
 	}
-	 
+
 }
